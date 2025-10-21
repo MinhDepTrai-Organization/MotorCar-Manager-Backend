d7210550205d7f03b0528cad7d505f03ffa77bb2
duynghia <duynghia0705@gmail.com>
2025-10-21 20:50:17 +0700

    update .gitignore

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello, Minhdeptrai is here';
+    return 'Welcome to Minh dep trai!!!!';
   }
 }
